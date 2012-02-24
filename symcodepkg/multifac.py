9bdccb3985774d70577d52741f3b8c63f2020125
Rense Lange <renselange@gmail.com>
2012-02-24 01:38:01 -0600

    save

--- symcodepkg/multifac.py
+++ symcodepkg/multifac.py
@@ -9,7 +9,7 @@ itembank = loaditemdat.loadfile('../data/itemdefs.txt')
 class Multifactor:
     
     def __init__(self,facprob):  
-    # facprob supposed to look like: {'Fluency':0.3,'Spatial':0.5,'Reasoning': 0.2} and a common factor '*' will always be pushed at start
+    # facprob supposed to look like: {'Fluency':0.3,'Spatial':0.5,'Reasoning': 0.2}, and additional common factor '*' will always be pushed at start
     
         self.facprob = [('*',0.0,0,Factor())] + [(Name,Prob,0,Factor()) for Name,Prob in facprob.iteritems()]
         
@@ -31,11 +31,13 @@ class Multifactor:
     # return index value, as well as factor itself
         return len(facprob)-1,self.facprob[-1]      # we might get here due to accumulated rounding errors
     
-    # find the factor whose selection brings observed and wanted proportions max closer  
+    # find the factor whose selection brings observed and wanted proportions max closer 
+    # the sequence is deterministic, given the first few (3?) randomly selected items
+    # EX: for six subfactors, this yields 3C6 possible orders = 120/6 = 20, good enough ...
       
     def needyfac(self):
         if self.count < 3: 
-            at,fac  = self.randfac() # first few times around is random
+            at,fac  = self.randfac() # first few times around select at random
         else:
             min = 2.0
             fac = None
@@ -48,8 +50,7 @@ class Multifactor:
                     at  = fi+1  # index into list - was off by 1
                 
     # the fields are: (<0=sub-area>,<1=desired-prop>,<2=actual-count>,<3=simple-factor>) 
-        self.facprob[at] = (fac[0],fac[1],fac[2]+1,fac[3]) # replace tuple
-        print at,self.facprob[at]
+        self.facprob[at] = (fac[0],fac[1],fac[2]+1,fac[3]) # kludge to replace tuple, use lists?
         return fac
         
     def addobs(self,item,obs=-1):
@@ -65,13 +66,7 @@ frq = {'Fluency':0,'Spatial':0,'Reasoning': 0}
 
 for i in range(40):
     f = m.needyfac()
+    print f[0],
     m.addobs(Item(0,0.0,[0.0,0.0],cat=f[0]))
-    print m.facprob
-    print
     frq[f[0]] += 1
-    '''print
-    for v in frq.keys(): print '(%s,%4.2f)'%(v,float(frq[v])/len(m.comfac.answered)),
-    print'''
-print;print    
-print frq
-        
+    print frq
