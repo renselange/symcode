26ffc5beb8f4f5bd8d2e99e835f25b33c95c439f
Rense Lange <renselange@gmail.com>
2012-02-24 00:58:48 -0600

    save

--- symcodepkg/multifac.py
+++ symcodepkg/multifac.py
@@ -10,23 +10,29 @@ class Multifactor:
     
     def __init__(self,facprob):  
     # facprob supposed to look like: {'Fluency':0.3,'Spatial':0.5,'Reasoning': 0.2} and a common factor '*' will always be pushed at start
+    
         self.facprob = [('*',0.0,0,Factor())] + [(Name,Prob,0,Factor()) for Name,Prob in facprob.iteritems()]
+        
     # so this will become to look like: [('*', 0.0, 0, <factor.Factor instance at 0x10377a6c8>), ..., ('Reasoning', 0.2, 0, <factor.Factor instance at 0x10377a710>)]
-    # the first one is always '*', the order of the others is essentially random, depending on the hash function
-    # the fields are: (<sub-area>,<desired-prop>,<actual-count>,<simple-factor>)
+    # the first entry is always '*', the order of the others is essentially random, depending on the hash function
+    # the fields are: (<0=sub-area>,<1=desired-prop>,<2=actual-count>,<3=simple-factor>) 
+    
         self.comfac  = self.facprob[0][3] # quick reference to main factor
         self.count   = 0
     
-        
+    # randomly select one of the factor, regardless of anything ...   
+    
     def randfac(self): # used to make the first choice
         r = random()
         c = 0.0
         for fi,f in enumerate(self.facprob[1:]): # skip the first
             c += f[1]
-            if c > r: return fi+1,f
+            if c > r: return fi+1,f # increase fi because first element was omitted
     # return index value, as well as factor itself
-        return len(facprob)-1,self.facprob[-1]      # we might get to end due to accumulated rounding errors
-        
+        return len(facprob)-1,self.facprob[-1]      # we might get here due to accumulated rounding errors
+    
+    # find the factor whose selection brings observed and wanted proportions max closer  
+      
     def needyfac(self):
         if self.count < 3: 
             at,fac  = self.randfac() # first few times around is random
@@ -42,7 +48,7 @@ class Multifactor:
                     at  = fi+1  # index into list - was off by 1
                 
     # the fields are: (<0=sub-area>,<1=desired-prop>,<2=actual-count>,<3=simple-factor>) 
-        self.facprob[at] = (fac[0],fac[1],fac[2]+1,fac[3]) # increase count the hard way
+        self.facprob[at] = (fac[0],fac[1],fac[2]+1,fac[3]) # replace tuple
         print at,self.facprob[at]
         return fac
         
